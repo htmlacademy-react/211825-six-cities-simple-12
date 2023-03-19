708f4c77e98546164e4ff16519c59fc7fd901d3f
KSait0 <6wayofpain@gmail.com>
2023-03-19 22:48:15 +0300

    Создал первые React-компоненты

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,5 +1,13 @@
-function App(): JSX.Element {
-  return <p>Hello, world!</p>;
+import MainPage from '../../pages/main-page/main-page';
+
+type AppProps = {
+  foundPlacesCounter: number;
+}
+
+function App({foundPlacesCounter}: AppProps): JSX.Element {
+  return (
+    <MainPage foundPlacesCounter={foundPlacesCounter} />
+  );
 }
 
 export default App;
--- project/src/index.tsx
+++ project/src/index.tsx
@@ -2,12 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/app/app';
 
+const Setting = {
+  FoundPlacesCount: 312,
+} as const;
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
 );
 
 root.render(
   <React.StrictMode>
-    <App />
+    <App
+      foundPlacesCounter = {Setting.FoundPlacesCount}
+    />
   </React.StrictMode>,
 );
